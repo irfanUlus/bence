a2d881abce05b3ab1c5c38287f1f12061ecc3457
iulus <irfanulus34@gmail.com>
2024-02-12 18:25:19 +0300

    birinç

---
+++ src/p01.java
@@ -0,0 +1,20 @@
+public class p01 {
+
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("1.");
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/p05.java
@@ -0,0 +1,6 @@
+public class p05 {
+    public static void main(String[] args) {
+        System.out.println("5.");
+
+    }
+}
